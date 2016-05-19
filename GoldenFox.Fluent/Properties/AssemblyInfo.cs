4ab6a25f7b2cf7e9e2a4f69c279a17c09ed211ff
Mattias Nordqvist <mattias.nordqvist@gmail.com>
2016-05-19 15:54:02 +0200

    nuspec for fluent api

--- GoldenFox.Fluent/Properties/AssemblyInfo.cs
+++ GoldenFox.Fluent/Properties/AssemblyInfo.cs
@@ -1,36 +1,17 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("GoldenFox.Fluent")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Fluent Golden Fox")]
+[assembly: AssemblyDescription("Foxy scheduling")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("GoldenFox.Fluent")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyCopyright("Copyright © Mattias Nordqvist")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AssemblyVersion("2.2.1.1")]
+[assembly: AssemblyFileVersion("2.2.1.1")]
+[assembly: AssemblyProduct("Fluent Golden Fox")]
+[assembly: AssemblyCompany("Mattias Nordqvist")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("814a5117-9338-48d4-967b-65fb67811842")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
